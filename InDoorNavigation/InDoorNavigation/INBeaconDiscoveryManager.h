fb3dea4ca31e031ccd1d4a10c5786e641480d474
Alex Kalinichenko <akalinichenko@lixar.com>
2015-11-14 15:22:25 -0400

    Add logic to BeaconDiscoveryManager for getting the nearest beacon.

--- InDoorNavigation/InDoorNavigation/INBeaconDiscoveryManager.h
+++ InDoorNavigation/InDoorNavigation/INBeaconDiscoveryManager.h
@@ -14,6 +14,7 @@ extern NSString * const INBeaconDiscoveryManagerDidUpdateDiscoveredBeacons;
 @interface INBeaconDiscoveryManager : NSObject
 
 @property (nonatomic, readonly) NSArray *discoveredBeacons;
+@property (nonatomic, readonly) INBeacon *nearestBeacon;
 
 + (instancetype)sharedManager;
 
